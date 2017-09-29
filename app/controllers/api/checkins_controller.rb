7acf6e1949893dc1afd1838902ec32f643ec6e05
Clay Shaw <Claydshaw@gmail.com>
2017-09-29 11:28:10 -0700

    Add current user check to api call to prevent other users from updating checkins

--- app/controllers/api/checkins_controller.rb
+++ app/controllers/api/checkins_controller.rb
@@ -20,7 +20,7 @@ class Api::CheckinsController < ApplicationController
 
   def update
     @checkin = Checkin.find(params[:id])
-    if @checkin.update(checkin_params)
+    if @checkin.update(checkin_params) && @checkin.user.username === current_user.username
       render :show
     else
       render json: @checkin.errors.full_messages, status: 422
