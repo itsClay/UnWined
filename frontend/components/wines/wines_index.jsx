fcad0fe609a77af5b4bd8e407120c7a75924e502
Clay Shaw <Claydshaw@gmail.com>
2017-11-10 15:27:04 -0800

    Implement search filter for wines

--- frontend/components/wines/wines_index.jsx
+++ frontend/components/wines/wines_index.jsx
@@ -7,20 +7,46 @@ import { ProtectedRoute } from '../../util/route_util';
 class WinesIndex extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      search: ''
+    }
+    this.updateWineSearch = this.updateWineSearch.bind(this)
   }
 
   componentDidMount() {
     this.props.fetchWines();
   }
 
+  updateWineSearch(e) {
+    e.preventDefault()
+    this.setState({search: e.target.value})
+    console.log(e.target.value)
+  }
+
   render() {
-    const wines = this.props.wines;
+    // let filteredWines = this.props.wines;
+
+    const wines = this.props.wines.filter(
+      (wine) => {
+        if (wine.vineyard.toLowerCase().indexOf(this.state.search.toLowerCase()) !== -1 || 
+          wine.wine_type.toLowerCase().indexOf(this.state.search.toLowerCase()) !== -1) {
+            return wine;
+          }
+      }
+    );
     return (
       <div className="wines-container">
 
         <div className="wine-idx-container">
           <div className="wine-idx-header">
             <span>Wines</span>
+            <div className="wine-search">
+              <input type="text" 
+                value={this.state.search} 
+                onChange={this.updateWineSearch}
+                placeholder='Search Wines...'
+              />
+            </div>
             <Link to="/wines/new">
               <div className="input-btn light-purple">
                 <button>Make a Wine</button>
