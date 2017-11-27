2452d740a87b536fcdd22fffab044b448c36ac07
Clay Shaw <Claydshaw@gmail.com>
2017-11-27 14:07:39 -0800

    Refactor rendering of results to only be rendered on use

--- frontend/components/search/search.jsx
+++ frontend/components/search/search.jsx
@@ -13,39 +13,55 @@ class Search extends React.Component {
     }
     this.handleChange = this.handleChange.bind(this)
     this.handleSearch = debounce(() => props.fetchSearch(this.state.search), 1000).bind(this)
+    this.displayResults = this.displayResults.bind(this)
   }
 
   handleChange(e) {
-    this.setState({search: e.target.value})
-    this.handleSearch()
+    this.setState({search: e.target.value});
+    this.handleSearch();
+  }
+
+  displayResults(users, wines) {
+    if(this.state.search === '') {
+      return;
+    } else {
+      return (
+        <ul className="search-result">
+          <div className="search-title">
+            <li>Users</li>
+            <li className="sub">({users.length})</li>
+          </div>
+          {
+            this.props.query.users &&
+            <SearchResultsList users={users} type="users" />
+          }
+          <div className="search-title">
+            <li>Wines</li>
+            <li className="sub">({wines.length})</li>
+          </div>
+          {
+            this.props.query.wines &&
+            <SearchResultsList wines={wines} type="wines"/>
+          }
+        </ul>
+      )
+    }
   }
 
   render() {
-    console.log(this)
     const users = this.props.query.users ? this.props.query.users : []
     const wines = this.props.query.wines ? this.props.query.wines : []
 
     return (
       <div>
         <input
-          type="search"
           placeholder="search"
           value={this.state.search}
           onChange={this.handleChange}
+          onFocus={this.displayResults}
+          className="search-input"
         />
-      <ul className="search-result">
-          <li className="search-title">Users</li>
-          {
-            this.props.query.users &&
-            <SearchResultsList users={users} type="users" />
-          }
-
-          <li className="search-title">Wines</li>
-          {
-            this.props.query.wines &&
-            <SearchResultsList wines={wines} type="wines"/>
-          }
-        </ul>
+      {this.displayResults(users, wines)}
       </div>
     )
   }
